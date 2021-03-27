c83a057e3c885be124c2def66426f9c05ebc557d
Sigma086 <vineetmahajan2000@gmail.com>
2021-03-27 17:42:04 +0530

    .

--- mysite/api/urls.py
+++ mysite/api/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 
 from . import views
 
-urlpatterns = [
+urlpatterns = format_suffix_patterns([
+    path('', views.BoardList.as_view()),
+    path('<int:pk>/', views.BoardDetail.as_view()),
 
-]
+])
+

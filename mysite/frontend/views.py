55fa32923c2112dfbebc1a250cfb9e5ecdf8ad66
Sigma086 <vineetmahajan2000@gmail.com>
2021-03-27 23:07:10 +0530

    Room page

--- mysite/frontend/views.py
+++ mysite/frontend/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import get_list_or_404, get_object_or_404, redirect, render
+from django.contrib.auth import logout
 
 from api.models import Board, Pin
 
@@ -10,7 +12,20 @@ def get_common():
 def home_view(requests):
     context = get_common()
 
-    print(context)
     return render(requests, 'frontend/home.html', context)
 
+def room_view(requests, pk):
+    context                 = get_common()
+    context['Room']         = get_object_or_404(Board, id=pk)
+    context['appliances']   = get_list_or_404(Pin, board__id=pk)
 
+    return render(requests, 'frontend/room.html', context)
+
+
+def login_view(request):
+    return HttpResponse('login')
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('home')

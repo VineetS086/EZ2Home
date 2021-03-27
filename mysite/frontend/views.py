305bc7cc4aa264192d4ea7a4e178ff78eaff487e
Sigma086 <vineetmahajan2000@gmail.com>
2021-03-27 21:07:45 +0530

    home page integration 1

--- mysite/frontend/views.py
+++ mysite/frontend/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+from api.models import Board, Pin
+
+def get_common():
+    return{
+        'rooms':Board.objects.all()
+    }
+
+def home_view(requests):
+    context = get_common()
+
+    print(context)
+    return render(requests, 'frontend/home.html', context)
+
+
